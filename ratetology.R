65f113748c8402f8f3b1259f236f46ad134b691c
aivelo <tuomas.aivelo@helsinki.fi>
2025-05-19 15:42:44 +0200

    Update ratetology.R

--- ratetology.R
+++ ratetology.R
@@ -1,6 +1,6 @@
 #download and load necessary packages
 
-deps = c("car", "ggplot2","lme4","dplyr","tidyr","psych","plyr", "corrr");
+deps = c("car", "ggplot2","lme4","dplyr","tidyr","psych","plyr", "corrr","multcomp");
 
 for (dep in deps){
   if (dep %in% installed.packages()[,"Package"] == FALSE){
@@ -32,12 +32,14 @@ plot(cb1)
 anova(cb1)
 summary(cb1)
 with(summary(cb1), 1 - deviance/null.deviance)
+drop1(cb1, test="Chisq")
 
 cb2 <- glm(other_pos ~ duration+age+ind_count+sex, data=df, family=gaussian) # ind_count, agejuv
 plot(cb2)
 anova(cb2)
 summary(cb2)
 with(summary(cb2), 1 - deviance/null.deviance)
+drop1(cb2, test="Chisq")
 
 
 cb3 <- glm(other_neg ~ duration+age+ind_count+sex, data=df, family=gaussian) # ageboth, agejuv
@@ -45,74 +47,76 @@ plot(cb3)
 anova(cb3)
 summary(cb3)
 with(summary(cb3), 1 - deviance/null.deviance)
+drop1(cb3, test="Chisq")
+summary(glht(cb3, mcp(age="Tukey")))
 
 
-#graphs
-
-
-df$div <- df[,7]+df[,8]+df[,9]+df[,10]+df[,11]
-
-data1 <- gather(df, behaviour, time, leaves_pos:other_neg, factor_key=TRUE)
-
-data1$timeperc <- (data1$time/data1$div)*100
-
-#Fig 3A
-
-means <- ddply(data1, c("behaviour", "age"), summarise,
-               mean=mean(timeperc))
-
-means.sem <- ddply(data1, c("behaviour", "age"), summarise,
-                   mean=mean(timeperc), sem=sd(timeperc)/sqrt(length(timeperc)))
-means.sem <- transform(means.sem, lower=mean-sem, upper=mean+sem)
-
-means.sem[means.sem$behaviour=='leaves_pos',5:6] <- means.sem[means.sem$behaviour=='forage_pos',3]+
-  means.sem[means.sem$behaviour=='other_pos',3]+ means.sem[means.sem$behaviour=='leaves_neg',3]+
-  means.sem[means.sem$behaviour=='other_neg',3]+ means.sem[means.sem$behaviour=='leaves_pos',5:6]
-
-means.sem[means.sem$behaviour=='forage_pos',5:6] <-means.sem[means.sem$behaviour=='other_pos',3]+ 
-  means.sem[means.sem$behaviour=='leaves_neg',3]+ means.sem[means.sem$behaviour=='other_neg',3]+ 
-  means.sem[means.sem$behaviour=='forage_pos',5:6]
-
-means.sem[means.sem$behaviour=='other_pos',5:6] <- means.sem[means.sem$behaviour=='leaves_neg',3]+
-  means.sem[means.sem$behaviour=='other_neg',3]+ means.sem[means.sem$behaviour=='other_pos',5:6]
-
-means.sem[means.sem$behaviour=='leaves_neg',5:6] <-means.sem[means.sem$behaviour=='other_neg',3]+ 
-  means.sem[means.sem$behaviour=='leaves_neg',5:6]
-
-
-ggplot(means.sem, aes(fill=behaviour, y=mean, x=age)) + 
-              geom_bar(stat="identity")+ 
-              geom_errorbar(aes(ymax=upper,  ymin=lower),width=0.5)
-
-#Fig3B
-
-means <- ddply(data1, c("behaviour", "ind_count"), summarise,
-               mean=mean(timeperc))
-
-means.sem <- ddply(data1, c("behaviour", "ind_count"), summarise,
-                   mean=mean(timeperc), sem=sd(timeperc)/sqrt(length(timeperc)))
-means.sem <- transform(means.sem, lower=mean-sem, upper=mean+sem)
-
-means.sem[means.sem$behaviour=='leaves_pos',5:6] <- means.sem[means.sem$behaviour=='forage_pos',3]+
-  means.sem[means.sem$behaviour=='other_pos',3]+ means.sem[means.sem$behaviour=='leaves_neg',3]+
-  means.sem[means.sem$behaviour=='other_neg',3]+ means.sem[means.sem$behaviour=='leaves_pos',5:6]
-
-means.sem[means.sem$behaviour=='forage_pos',5:6] <-means.sem[means.sem$behaviour=='other_pos',3]+ 
-  means.sem[means.sem$behaviour=='leaves_neg',3]+ means.sem[means.sem$behaviour=='other_neg',3]+ 
-  means.sem[means.sem$behaviour=='forage_pos',5:6]
-
-means.sem[means.sem$behaviour=='other_pos',5:6] <- means.sem[means.sem$behaviour=='leaves_neg',3]+
-  means.sem[means.sem$behaviour=='other_neg',3]+ means.sem[means.sem$behaviour=='other_pos',5:6]
-
-means.sem[means.sem$behaviour=='leaves_neg',5:6] <-means.sem[means.sem$behaviour=='other_neg',3]+ 
-  means.sem[means.sem$behaviour=='leaves_neg',5:6]
-
-
-ggplot(means.sem, aes(fill=behaviour, y=mean, x=ind_count)) + 
-  geom_bar(stat="identity")+ 
-  geom_errorbar(aes(ymax=upper,  ymin=lower),width=0.5)
-
-
+##removed graphs - stacked bar graphs with error bars for continuous behaviour
+#
+#
+#df$div <- df[,7]+df[,8]+df[,9]+df[,10]+df[,11]
+#
+#data1 <- gather(df, behaviour, time, leaves_pos:other_neg, factor_key=TRUE)
+#
+#data1$timeperc <- (data1$time/data1$div)*100
+#
+#Fig 3A rev1
+#
+#means <- ddply(data1, c("behaviour", "age"), summarise,
+#               mean=mean(timeperc))
+#
+#means.sem <- ddply(data1, c("behaviour", "age"), summarise,
+#                   mean=mean(timeperc), sem=sd(timeperc)/sqrt(length(timeperc)))
+#means.sem <- transform(means.sem, lower=mean-sem, upper=mean+sem)
+#
+#means.sem[means.sem$behaviour=='leaves_pos',5:6] <- means.sem[means.sem$behaviour=='forage_pos',3]+
+#  means.sem[means.sem$behaviour=='other_pos',3]+ means.sem[means.sem$behaviour=='leaves_neg',3]+
+#  means.sem[means.sem$behaviour=='other_neg',3]+ means.sem[means.sem$behaviour=='leaves_pos',5:6]
+#
+#means.sem[means.sem$behaviour=='forage_pos',5:6] <-means.sem[means.sem$behaviour=='other_pos',3]+ 
+#  means.sem[means.sem$behaviour=='leaves_neg',3]+ means.sem[means.sem$behaviour=='other_neg',3]+ 
+#  means.sem[means.sem$behaviour=='forage_pos',5:6]
+#
+#means.sem[means.sem$behaviour=='other_pos',5:6] <- means.sem[means.sem$behaviour=='leaves_neg',3]+
+#  means.sem[means.sem$behaviour=='other_neg',3]+ means.sem[means.sem$behaviour=='other_pos',5:6]
+#
+#means.sem[means.sem$behaviour=='leaves_neg',5:6] <-means.sem[means.sem$behaviour=='other_neg',3]+ 
+#  means.sem[means.sem$behaviour=='leaves_neg',5:6]
+#
+#
+#ggplot(means.sem, aes(fill=behaviour, y=mean, x=age)) + 
+#              geom_bar(stat="identity")+ 
+#              geom_errorbar(aes(ymax=upper,  ymin=lower),width=0.5)
+#
+#Fig3B rev1
+#
+#means <- ddply(data1, c("behaviour", "ind_count"), summarise,
+#               mean=mean(timeperc))
+#
+#means.sem <- ddply(data1, c("behaviour", "ind_count"), summarise,
+#                   mean=mean(timeperc), sem=sd(timeperc)/sqrt(length(timeperc)))
+#means.sem <- transform(means.sem, lower=mean-sem, upper=mean+sem)
+#
+#means.sem[means.sem$behaviour=='leaves_pos',5:6] <- means.sem[means.sem$behaviour=='forage_pos',3]+
+#  means.sem[means.sem$behaviour=='other_pos',3]+ means.sem[means.sem$behaviour=='leaves_neg',3]+
+#  means.sem[means.sem$behaviour=='other_neg',3]+ means.sem[means.sem$behaviour=='leaves_pos',5:6]
+#
+#means.sem[means.sem$behaviour=='forage_pos',5:6] <-means.sem[means.sem$behaviour=='other_pos',3]+ 
+#  means.sem[means.sem$behaviour=='leaves_neg',3]+ means.sem[means.sem$behaviour=='other_neg',3]+ 
+#  means.sem[means.sem$behaviour=='forage_pos',5:6]
+#
+#means.sem[means.sem$behaviour=='other_pos',5:6] <- means.sem[means.sem$behaviour=='leaves_neg',3]+
+#  means.sem[means.sem$behaviour=='other_neg',3]+ means.sem[means.sem$behaviour=='other_pos',5:6]
+#
+#means.sem[means.sem$behaviour=='leaves_neg',5:6] <-means.sem[means.sem$behaviour=='other_neg',3]+ 
+#  means.sem[means.sem$behaviour=='leaves_neg',5:6]
+#
+#
+#ggplot(means.sem, aes(fill=behaviour, y=mean, x=ind_count)) + 
+#  geom_bar(stat="identity")+ 
+#  geom_errorbar(aes(ymax=upper,  ymin=lower),width=0.5)
+#
+#
 # point behaviour - differences between classes
 
 df$agon  <- df$chase+df$wary+df$steal+df$wrestle
@@ -138,20 +142,23 @@ plot(pb1)
 anova(pb1)
 summary(pb1)
 with(summary(pb1), 1 - deviance/null.deviance)
-
+drop1(pb1, test="Chisq")
 
 pb2 <- glm(smelling ~ duration+age+ind_count+sex, data=df, family=binomial) # age:both
 plot(pb2)
 anova(pb2)
 summary(pb2)
 with(summary(pb2), 1 - deviance/null.deviance)
-
+drop1(pb2, test="Chisq")
+summary(glht(pb2, mcp(age="Tukey")))
 
 pb3 <- glm(touch ~ duration+age+ind_count+sex, data=df, family=binomial) #ind_count
 plot(pb3)
 anova(pb3)
 summary(pb3)
 with(summary(pb3), 1 - deviance/null.deviance)
+drop1(pb3, test="Chisq")
+summary(glht(pb3, mcp(age="Tukey")))
 
 
 #no sense testing play
@@ -161,17 +168,25 @@ plot(pb4)
 anova(pb4)
 summary(pb4)
 with(summary(pb4), 1 - deviance/null.deviance)
+drop1(pb4, test="Chisq")
+summary(glht(pb4, mcp(age="Tukey")))
 
 
 pb6 <- glm(chase ~ duration+age+ind_count+sex, data=df, family=binomial)
-plot(pb2)
-anova(pb2)
+plot(pb6)
+anova(pb6)
 summary(pb6)
+drop1(pb6, test="Chisq")
+
 
 pb7 <- glm(wary ~ duration+age+ind_count+sex, data=df, family=binomial) 
-plot(pb2)
-anova(pb2)
+plot(pb7)
+anova(pb7)
 summary(pb7)
+with(summary(pb7), 1 - deviance/null.deviance)
+
+drop1(pb7, test="Chisq")
+
 
 #no sense testing steal or wrestle
 
@@ -230,7 +245,7 @@ ggplot(df, aes(x = ind_count, y = smelling, color=age, fill=age)) +
   theme(panel.background = element_blank())
 
 
-#Fig 5c - smelling
+#Fig 4c - touching
 
 preds_a <- predict(pb3,newdata=pd_a, type="response", se.fit=TRUE)
 pd_a$fit <- preds_a$fit
@@ -252,3 +267,26 @@ ggplot(df, aes(x = ind_count, y = smelling, color=age, fill=age)) +
               alpha = 0.2, linetype=0) +
   geom_line(data = pd, aes(y = fit,color=age)) + 
   theme(panel.background = element_blank())
+
+#Fig 4d - sharing
+
+preds_a <- predict(pb1,newdata=pd_a, type="response", se.fit=TRUE)
+pd_a$fit <- preds_a$fit
+pd_a$se = preds_a$se.fit
+
+preds_b <- predict(pb1,newdata=pd_b, type="response", se.fit=TRUE)
+pd_b$fit <- preds_b$fit
+pd_b$se = preds_b$se.fit
+
+preds_juv <- predict(pb1,newdata=pd_juv, type="response", se.fit=TRUE)
+pd_juv$fit <- preds_juv$fit
+pd_juv$se = preds_juv$se.fit
+
+pd <- bind_rows(pd_a, pd_b, pd_juv)
+
+ggplot(df, aes(x = ind_count, y = sharing, color=age, fill=age)) +
+  geom_point(position = position_jitter(width = 0.05, height = 0)) +
+  geom_ribbon(data = pd, aes(y = fit, ymin = fit - 1.96 * se, ymax = fit + 1.96 * se), 
+              alpha = 0.2, linetype=0) +
+  geom_line(data = pd, aes(y = fit,color=age)) + 
+  theme(panel.background = element_blank())sharomg
